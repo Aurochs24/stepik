2026c097c710ea2acc8ed8fd5ed2f21c59115a70
Aurochs24 <aurochs@ukr.net>
2017-07-12 18:08:22 +0300

    Delete 5.py

--- 5.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-year, month, day = input().split()
-date = datetime.date( int( year ), int( month ), int( day ) )
-delta = datetime.timedelta( days = float( input()))
-date = date + delta
-print( date.year, date.month, date.day )
